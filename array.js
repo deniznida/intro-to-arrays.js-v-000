e01245321b4894f2c29831911fd484b5902533df
deniznida <deniz@flatironschool.com>
2016-05-13 14:34:27 -0400

    done

--- array.js
+++ array.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function createArray(){
+  return ["snickers", "hundred grand", "kitkat", "skittles"];
+}
+
+function addElementToArray(movies) {
+   movies.push('A Christmas Story');
+   return movies;
+}
+
+function accessElementFromArray(arr){
+  return arr[2];
+}
+
+function replaceElementInArray(names) {
+  names[1] = "Carter";
+  return names[1];
+}
+
+function removeElementFromArray(dishes) {
+  dishes.splice(2,2, "Roast Chicken");
+  return dishes;
+}
+
+function iterateArray(numbers){
+  var newNums = [];
+  numbers.forEach(function(number){
+    number = number + 5;
+    newNums.push(number);
+  });
+  return newNums;
+}
